c3bb031f315d580ca9fb0331f2404624b9e6cbe7
jared <66798137@qq.com>
2013-08-08 23:36:20 +0800

    create helloworld app
    create helloworld app

---
+++ Helloworld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Helloworld
+//
+//  Created by jared on 13-8-7.
+//  Copyright jared 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
